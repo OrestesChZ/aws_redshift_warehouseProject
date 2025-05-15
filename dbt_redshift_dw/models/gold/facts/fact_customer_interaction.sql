f71f09a8fdf364cda47f7496cfda86aa670bb0ab
OrestesChZ <oreschz@hotmail.com>
2025-05-14 23:58:04 -0500

    upd

--- dbt_redshift_dw/models/gold/facts/fact_customer_interaction.sql
+++ dbt_redshift_dw/models/gold/facts/fact_customer_interaction.sql
@@ -23,9 +23,28 @@ WITH source_data AS (
 )
 
 SELECT
-
-FROM source_data s
-INNER JOIN {{ref('dim_date')}} as d on s.date_id = d.date_id
-INNER JOIN {{ref('dim_customer')}} as d on s.date_id = d.date_id
-INNER JOIN {{ref('dim_location')}} as d on s.date_id = d.date_id
-INNER JOIN {{ref('dim_channel')}} as d on s.date_id = d.date_id
+    s.date_id,
+    s.interaction_id,
+    d.date as interaction_date,
+    s.customer_id,
+    c.first_name,
+    c.last_name,
+    c.full_name,
+    c.city,
+    c.state,
+    l,country,
+    c.postal_code,
+    s.channel_id,
+    ch.channel_name,
+    s.interaction_type,
+    s.interaction_rating
+FROM
+    source_data s
+INNER JOIN
+    {{ ref('dim_date') }} as d ON s.date_id = d.date_id
+INNER JOIN
+    {{ ref('dim_customer') }} as c ON s.customer_id = c.customer_id
+INNER JOIN
+    {{ ref('dim_location') }} as l ON s.location_id = l.location_id
+INNER JOIN
+    {{ ref('dim_channel') }} as ch ON s.channel_id = ch.channel_id
